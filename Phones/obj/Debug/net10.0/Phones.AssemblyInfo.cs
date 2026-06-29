fe91108fe96c0192d299cbb1752e53fd43186d57
Madiyar <tolkybekmadiar@gmail.com>
2026-06-29 21:12:14 +0500

    Installed 'Entity framework packages

--- Phones/obj/Debug/net10.0/Phones.AssemblyInfo.cs
+++ Phones/obj/Debug/net10.0/Phones.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Phones")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a8860673a2cacfa3a3485d584541db2179a9d0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4aa4befc60aa0d57c5c4c03aec6fc86aa824396d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Phones")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Phones")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
