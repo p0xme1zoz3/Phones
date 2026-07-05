9b1e4183f84f3cc6d13e4bcd662b2603f9181572
Madiyar <tolkybekmadiar@gmail.com>
2026-07-05 21:06:03 +0500

    fix: moved db to data

--- Phones/obj/Debug/net10.0/Phones.AssemblyInfo.cs
+++ Phones/obj/Debug/net10.0/Phones.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Phones")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3edddb472bd3b833da737509ee3eb151f8d7f29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f829fe4d11e4b9c5734aa3f20598a877ea75026")]
 [assembly: System.Reflection.AssemblyProductAttribute("Phones")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Phones")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
